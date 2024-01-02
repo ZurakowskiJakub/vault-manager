c9fe989f6a099a9c539bb5c41bf9e78a7f473eaf
Jakub Zurakowski <Jakub_zzzz@hotmail.com>
2024-01-02 12:27:41 +0000

    C00205431: Starting on tests

--- test/VaultManager.t.sol
+++ test/VaultManager.t.sol
@@ -7,15 +7,17 @@ import {VaultManager} from "../src/VaultManager.sol";
 contract VaultManagerTest is Test {
     VaultManager public vaultManager;
 
-    // function setUp() public {
-    //     vaultManager = new VaultManager();
-    //     vaultManager.setNumber(0);
-    // }
+    function setUp() public {
+        vaultManager = new VaultManager();
+    }
 
-    // function test_Increment() public {
-    //     vaultManager.increment();
-    //     assertEq(vaultManager.number(), 1);
-    // }
+    function testAddVault() public {
+        uint256 index = vaultManager.addVault();
+
+        assertEq(index == 0);
+        assertEq(vaultManager.vaults.length == 1);
+        assertEq(vaultManager.vaultsByOwner[msg.sender].length == 1);
+    }
 
     // function testFuzz_SetNumber(uint256 x) public {
     //     vaultManager.setNumber(x);
