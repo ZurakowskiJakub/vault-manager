d84e056610e223519b6aa9b84fa177ed7deeea0a
Jakub Zurakowski <Jakub_zzzz@hotmail.com>
2024-01-02 14:07:42 +0000

    C00205431: Unit Tests

--- test/VaultManager.t.sol
+++ test/VaultManager.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {VaultManager} from "../src/VaultManager.sol";
+import {Vault} from "../src/Vault.sol";
 
 contract VaultManagerTest is Test {
     VaultManager public vaultManager;
